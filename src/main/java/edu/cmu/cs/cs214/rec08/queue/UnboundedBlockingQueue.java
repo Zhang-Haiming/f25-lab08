820cd4cd46847e11518da9c6e01a06c517e7afdf
miaol <miaol@miaoldeMacBook-Pro-3.local>
2025-10-24 10:00:08 -0400

    Implement blocking queue

--- src/main/java/edu/cmu/cs/cs214/rec08/queue/UnboundedBlockingQueue.java
+++ src/main/java/edu/cmu/cs/cs214/rec08/queue/UnboundedBlockingQueue.java
@@ -22,14 +22,26 @@ public class UnboundedBlockingQueue<E> implements SimpleQueue<E> {
 
     public E peek() { return queue.peek(); }
 
-    public void enqueue(E element) { queue.add(element); }
+    public synchronized void enqueue(E element) {
+        queue.add(element);
+        notifyAll();
+    }
 
     /**
      * TODO:  Change this method to block (waiting for an enqueue) rather
      * than throw an exception.  Completing this task may require
      * modifying other methods.
      */
-    public E dequeue() { return queue.remove(); }
+    public synchronized E dequeue() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return queue.remove();
+    }
 
     public String toString() { return queue.toString(); }
 }
